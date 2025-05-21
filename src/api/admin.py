2c92bc3dac52525149411fa9ced01a194ecababe
Oscar Andres Bolaños Chacon <totmom07@gmail.com>
2025-05-21 00:27:47 +0000

    Errores

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,13 +1,16 @@
 import os
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
-from .models import db, User, Book, Favorite
+from .models import db, User, ExploreBook, PersonalBook, Favorite, Review
+
 def setup_admin(app):
     app.secret_key = os.environ.get('FLASK_APP_KEY', 'sample key')
     app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
     admin = Admin(app, name='4Geeks Admin', template_mode='bootstrap3')
 
-    # Registrar todos los modelos
+    # Register all models
     admin.add_view(ModelView(User, db.session))
-    admin.add_view(ModelView(Book, db.session))
+    admin.add_view(ModelView(ExploreBook, db.session))
+    admin.add_view(ModelView(PersonalBook, db.session))
     admin.add_view(ModelView(Favorite, db.session))
+    admin.add_view(ModelView(Review, db.session))
